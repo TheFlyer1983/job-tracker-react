bcee8566d3b41ac0ac781d39200942fed7c4b412
theflyer1983 <theflyer1983@gmail.com>
2026-08-16 19:30:13 +0100

    Implement job details display with localStorage integration
    - Added useEffect to fetch job details from localStorage based on job ID.
    - Introduced state management for job data using useState.
    - Enhanced JobDetails component to display job title, company, location, notes, and status.
    - Included AppHeader for consistent navigation.

--- src/pages/JobDetails.tsx
+++ src/pages/JobDetails.tsx
@@ -1,14 +1,32 @@
 import { useParams, NavLink } from "react-router";
+import { useEffect, useState } from "react";
+import type { Job } from "../constants/jobs";
+import AppHeader from "../components/AppHeader";
 
 export default function JobDetails() {
   const { id } = useParams();
+  const [job, setJob] = useState<Job | null>(null);
+
+  useEffect(() => {
+    const jobs = JSON.parse(localStorage.getItem("jobs") ?? "[]");
+
+    const foundJob = jobs.find((job: Job) => job.id.toString() === id);
+
+    setJob(foundJob ?? null);
+  }, [id]);
+
   return (
     <>
+      <AppHeader />
       <NavLink to="/" className="underline mb-4">
         Back to Jobs
       </NavLink>
-      <h1>Job Details</h1>
       <p>Job ID: {id}</p>
+      <div>{job?.title}</div>
+      <div>{job?.company}</div>
+      <div>{job?.location}</div>
+      <div>{job?.notes}</div>
+      <div>{job?.status}</div>
     </>
   );
 }
